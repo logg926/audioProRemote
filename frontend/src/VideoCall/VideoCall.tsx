22a7a3d26363abeb9efdfe50a62cac09654d3d5c
log G Cheng <logg.com@gmail.com>
2020-08-30 09:45:19 +0800

    enable prettier

--- frontend/src/VideoCall/VideoCall.tsx
+++ frontend/src/VideoCall/VideoCall.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // Using import.
 import * as mediasoupClient from "mediasoup-client";
@@ -9,37 +9,90 @@ import {
   version,
   detectDevice,
   Device,
-  parseScalabilityMode
+  parseScalabilityMode,
 } from "mediasoup-client";
 
-import io from 'socket.io-client'
+import io from "socket.io-client";
+import { RoomClient, addListeners } from "./RoomClient";
 
-const socket = io('https://localhost:3016')
+const socket: any = io("https://localhost:3016");
 
-const VideoCall = ()=>{
+// navigator.mediaDevices.enumerateDevices().then(devices =>
+//     devices.forEach(device => {
 
-    
-    useEffect(()=>{
-        if(socket){
-            //連線成功在 console 中打印訊息
-            console.log('success connect!')
-            //設定監聽
-            initWebSocket()
-        }
-    },[socket])
+//         console.log(device)
+//     //   let el = null
+//     //   if ('audioinput' === device.kind) {
+//     //     el = audioSelect
+//     //   } else if ('videoinput' === device.kind) {
+//     //     el = videoSelect
+//     //   }
+//     //   if(!el) return
 
-    const initWebSocket = () => {
-        //對 getMessage 設定監聽，如果 server 有透過 getMessage 傳送訊息，將會在此被捕捉
-        if (!!socket){
-            socket.on('getMessage', (message: string) => {
-            console.log(message)
-        })}
-    }
+//     //   let option = document.createElement('option')
+//     //   option.value = device.deviceId
+//     //   option.innerText = device.label
+//     //   el.appendChild(option)
+//     })
+//   )
 
-    return <>
-    <input type='button' value='連線'  />
-    {/* <input type='button' value='送出訊息' onClick={sendMessage} /> */}
-    </>
+socket.request = function request(type: any, data = {}) {
+  return new Promise((resolve, reject) => {
+    socket.emit(type, data, (data: any) => {
+      if (data.error) {
+        reject(data.error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+let rc: any = null;
+
+function joinRoom(name: any, room_id: any) {
+  if (rc && rc.isOpen()) {
+    console.log("already connected to a room");
+  } else {
+    rc = new RoomClient(
+      localMedia,
+      remoteVideos,
+      remoteAudios,
+      window.mediasoupClient,
+      socket,
+      room_id,
+      name,
+      roomOpen
+    );
+    addListeners();
+  }
 }
+
+const VideoCall = () => {
+  useEffect(() => {
+    if (socket) {
+      //連線成功在 console 中打印訊息
+      console.log("success connect!");
+      //設定監聽
+      initWebSocket();
+    }
+  }, [socket]);
+
+  const initWebSocket = () => {
+    //對 getMessage 設定監聽，如果 server 有透過 getMessage 傳送訊息，將會在此被捕捉
+    if (!!socket) {
+      socket.on("getMessage", (message: string) => {
+        console.log(message);
+      });
+    }
+  };
+
+  return (
+    <>
+      <input type="button" value="連線" />
+      {/* <input type='button' value='送出訊息' onClick={sendMessage} /> */}
+    </>
+  );
+};
 
 export default VideoCall;
