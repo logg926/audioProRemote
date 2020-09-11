8769cb4b99f067eccc85476b9bd03bc97d015afc
log g <logg.com@gmail.com>
2020-09-11 20:32:33 +0800

    adding worklet

--- frontend/src/app/controlplay/play/play.component.ts
+++ frontend/src/app/controlplay/play/play.component.ts
@@ -10,22 +10,54 @@ export class PlayComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    var sound = new Howl({
+      src: ['assets/sound.mp3'],
+    });
+
+    const audioContext = Howler.ctx;
+    audioContext.audioWorklet
+      .addModule('assets/worklet/white-noise-processor.js')
+      .then(
+        () => {
+          const whiteNoiseNode = new AudioWorkletNode(
+            audioContext,
+            'white-noise-processor'
+          );
+
+          // send to speaker
+
+          Howler.masterGain.connect(whiteNoiseNode);
+          whiteNoiseNode.connect(audioContext.destination);
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+
+    this.mediaRecorder(audioContext, sound.play, sound.stop);
+  }
+
+  mediaRecorder(audioContext, playfn, stopfn) {
     Howler.mute(false);
-    let mediaDest = Howler.ctx.createMediaStreamDestination();
+
+    const mediaDest: MediaStreamAudioDestinationNode = audioContext.createMediaStreamDestination();
 
     Howler.masterGain.disconnect();
+    // Master Gain -> mediaDest
     Howler.masterGain.connect(mediaDest);
 
-    let audioChunks = [];
-
-    let mediaRecorder = new MediaRecorder(mediaDest.stream, {
+    const audioChunks = [];
+    const mediaRecorder = new MediaRecorder(mediaDest.stream, {
       mimeType: 'audio/webm',
     });
+
     mediaRecorder.onstart = (event) => {
       console.log('Started recording Howl output...');
     };
     mediaRecorder.ondataavailable = (e) => {
-      if (e.data.size) audioChunks.push(e.data);
+      if (e.data.size) {
+        audioChunks.push(e.data);
+      }
     };
     mediaRecorder.onstop = (event) => {
       console.log('Completed Recording', audioChunks); // why is this returning empty?
@@ -35,16 +67,12 @@ export class PlayComponent implements OnInit {
       // audioPlayer.play()
     };
 
-    var sound = new Howl({
-      src: ['assets/sound.mp3'],
-    });
-
-    sound.play();
+    playfn();
     mediaRecorder.start();
 
     setTimeout(() => {
       mediaRecorder.stop();
-      sound.stop();
+      stopfn();
     }, 5000);
   }
 }
